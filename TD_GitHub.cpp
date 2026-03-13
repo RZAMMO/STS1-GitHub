4bb99dee59d2b9343cdccd2b962c2a7f822cceee
lonymaz <lonymaz7@gmail.com>
2026-03-13 09:55:45 +0100

    Update TD_GitHub.cpp

--- TD_GitHub.cpp
+++ TD_GitHub.cpp
@@ -65,3 +65,78 @@ int plusGrand(int a, int b, int c);
 
 
 //Fonctions réalisées par l'étudiant 3 :
+
+//fonction qui verifie si il est pair ou divisible par 8
+bool estImpair(int nb) {
+    // Vérifie si n est impair ou divisible par 8
+    return (nb % 2 != 0) || (nb % 8 == 0);
+}
+
+int main() {
+    int nombre;
+
+    cout << "Entrez un nombre : ";
+    cin >> nombre;
+
+    if (estImpair(nombre)) {
+        cout << nombre << " est impair ou divisible par 8." << endl;
+    } else {
+        cout << nombre << " n'est ni impair ni divisible par 8." << endl;
+    }
+
+    return 0;
+}
+
+// Fonction qui retourne la moyenne de trois nombres
+double moyenne(double nb1, double nb2, double nb3) {
+    return (nb1 + nb2 + nb3) / 3.0;
+    double x, y, z;
+
+    cout << "Entrez trois nombres : ";
+    cin >> x >> y >> z;
+
+    cout << "La moyenne est : " << moyenne(x, y, z) << endl;
+
+    return 0;
+}
+
+// Fonction qui applique une réduction et ajoute la TVA de 5,5 %
+double reduction(int prix, int reduc) {
+    // Appliquer la réduction
+    double prixReduit = prix * (1 - reduc / 100.0);
+    
+    // Appliquer la TVA de 5,5%
+    double prixFinal = prixReduit * 1.055;
+    
+    return prixFinal;
+	
+    double prix, reductionPourc;
+
+    cout << "Entrez le prix : ";
+    cin >> prix;
+
+    cout << "Entrez le pourcentage de réduction : ";
+    cin >> reductionPourc;
+
+    cout << "Le prix final après réduction et TVA est : " 
+         << reduction(prix, reductionPourc) << " €" << endl;
+
+    return 0;
+}
+
+// Fonction qui retourne le nombre médian parmi trois nombres
+int nombreMedian(int a, int b, int c) {
+    if ((a >= b && a <= c) || (a >= c && a <= b))
+        return a;
+    else if ((b >= a && b <= c) || (b >= c && b <= a))
+        return b;
+    else
+        return c;
+    int x, y, z;
+    cout << "Entrez trois nombres : ";
+    cin >> x >> y >> z;
+
+    cout << "Le nombre médian est : " << nombreMedian(x, y, z) << endl;
+
+    return 0;
+}
